6ae4d6696a03aa99195127fc0bdbf01e52aed0df
Tran Minh <teemo2105@gmail.com>
2024-07-11 23:55:44 +0700

    fin

--- web/ulti/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize('minh8', 'root', 'meomeo21',{
-    dialect: 'mysql',
-    host: 'localhost'
-})
-
-module.exports = sequelize
